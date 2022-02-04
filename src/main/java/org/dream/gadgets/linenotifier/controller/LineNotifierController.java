9c278f370560f3056c27f097f1f4768cb2f5d4a0
George-Chou <george_chou@gss.com.tw>
2022-02-05 07:17:55 +0800

    send ordinary message

--- src/main/java/org/dream/gadgets/linenotifier/controller/LineNotifierController.java
+++ src/main/java/org/dream/gadgets/linenotifier/controller/LineNotifierController.java
@@ -1,13 +1,18 @@
 package org.dream.gadgets.linenotifier.controller;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.dream.gadgets.linenotifier.model.dto.AccessTokenResponse;
 import org.dream.gadgets.linenotifier.model.dto.AuthorizeCodeAndState;
+import org.dream.gadgets.linenotifier.model.dto.GenericResponse;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "/notifier")
 public class LineNotifierController {
     
+    private static final Map<String, String> tempTokenStore = new ConcurrentHashMap<>();
+
     @PostMapping(
         value = "callback",
         consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
@@ -53,7 +60,36 @@ public class LineNotifierController {
             tokenResponse.getStatus(),
             tokenResponse.getMessage()
         );
+
+        tempTokenStore.put("accessToken", tokenResponse.getAccessToken());
         ResponseEntity<String> response = ResponseEntity.ok("Token fetched");
+        return response;
+    }
+
+    @GetMapping(value = "sendMessage")
+    public ResponseEntity<String> sendMessage() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setBearerAuth(tempTokenStore.get("accessToken"));
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+        map.add("message", "line notifier test 123");
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<GenericResponse> tokenExchangeResponseEntity = restTemplate.postForEntity(
+            "https://notify-bot.line.me/api/notify",
+            entity,
+            GenericResponse.class
+        );
+
+        GenericResponse tokenResponse = tokenExchangeResponseEntity.getBody();
+        log.info(
+            "status: {}, message: {}", 
+            tokenResponse.getStatus(),
+            tokenResponse.getMessage()
+        );
+        ResponseEntity<String> response = ResponseEntity.ok("Message sent");
         return response;
     }
 
