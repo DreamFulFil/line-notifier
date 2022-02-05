9a1828e0dda505af2b3bf2c2cf4700cda8564e35
George-Chou <george_chou@gss.com.tw>
2022-02-05 08:17:42 +0800

    Make sure cause is not convenience method

--- src/main/java/org/dream/gadgets/linenotifier/controller/LineNotifierController.java
+++ src/main/java/org/dream/gadgets/linenotifier/controller/LineNotifierController.java
@@ -72,9 +72,9 @@ public class LineNotifierController {
         log.info("Fetched token from storage: {}", token);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer "+ token);
+        headers.setBearerAuth(token);
         MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
-        map.add("message", "line notifier test 444555666");
+        map.add("message", "line notifier test 777888999");
 
         HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
 
